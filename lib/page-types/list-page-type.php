230f30d07e58af8faca96fb2a4f5912181685a81
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-04-22 15:42:38 +0200

    Added list page type

---
+++ lib/page-types/list-page-type.php
@@ -0,0 +1,50 @@
+<?php
+
+class List_Page_Type extends Papi_Page_Type {
+
+	public function page_type() {
+		return [
+			'description' => 'Vel adipisicing dapibus nostra. Lectus malesuada volutpat aliquet',
+			'fill_labels' => true,
+			'name'        => 'List page',
+			'template'    => 'pages/list-page.php'
+		];
+	}
+
+	public function register() {
+
+		$this->remove([
+			'editor'
+		]);
+
+		$this->box('Content', [
+
+			papi_property( [
+				'title'    => 'Pages',
+				'type'     => 'repeater',
+				'sidebar'  => false,
+				'settings' => [
+					'items' => papi_property( [
+          	'title'    => 'Page',
+						'type'     => 'post',
+						'settings' => [
+							'text'      => '',
+							'post_type' => 'page',
+							'query'     => [
+								'meta_query' => [
+									[
+										'key' => '_papi_page_type',
+										'compare' => 'NOT EXISTS'
+									]
+								]
+							]
+						]
+					] )
+				]
+			] )
+
+		]);
+
+	}
+
+}
