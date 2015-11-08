fd9ffa9ae59f1cb534e247df7414a2880a3ac061
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-11-08 20:41:48 +0100

    Add FAQ post type

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
-	get_header();
-	while ( have_posts() ) : the_post();
-?>
 
-<div class="entry">
-	<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-	<div class="entry-text">
-		<?php the_content(); ?>
-	</div>
-</div>
-
-<?php
-	endwhile;
-	get_footer();
-?>
+echo '<pre>';
+var_dump(papi_get_option('link_test'));
+exit;
--- lib/custom-post-types.php
+++ lib/custom-post-types.php
@@ -45,3 +45,49 @@ function cpt_module() {
 }
 
 add_action( 'init', 'cpt_module' );
+
+/**
+ * Add custom post type `FAQ`.
+ */
+function cpt_faq() {
+  $labels = [
+	'name'               => 'FAQ',
+	'singular_name'      => 'FAQ',
+	'add_new'            => 'Add new',
+	'add_new_item'       => 'Add new FAQ',
+	'edit_item'          => 'Edit FAQ',
+	'new_item'           => 'Add new',
+	'all_items'          => 'All FAQs',
+	'view_item'          => 'Show FAQ',
+	'search_items'       => 'Search FAQs',
+	'not_found'          => 'No FAQs found',
+	'not_found_in_trash' => 'No FAQs found in the trash',
+	'parent_item_colon'  => '',
+	'menu_name'          => 'FAQs'
+  ];
+
+  $args = [
+	'menu_icon'          => 'dashicons-admin-page',
+	'labels'             => $labels,
+	'public'             => true,
+	'publicly_queryable' => true,
+	'show_ui'            => true,
+	'show_in_menu'       => true,
+	'query_var'          => true,
+	'capability_type'    => 'post',
+	'has_archive'        => false,
+	'hierarchical'       => false,
+	'menu_position'      => 10,
+	'can_export'         => true,
+	'rewrite'            => ['slug' => 'faq'],
+	'supports' 			=> [
+	  'title',
+	  'editor',
+	  'thumbnail'
+	]
+  ];
+
+  register_post_type( 'faq', $args );
+}
+
+add_action( 'init', 'cpt_faq' );
