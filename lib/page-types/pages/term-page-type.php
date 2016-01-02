0785a2572bdc73e6732bb802fea35c732eef1e07
Rasmus Bengtsson <rasmus@isotop.se>
2016-01-02 15:47:47 +0100

    Added page type to test term properties

---
+++ lib/page-types/pages/term-page-type.php
@@ -0,0 +1,65 @@
+<?php
+class Term_Page_Type extends Papi_Page_Type {
+
+	/**
+	 * Define our Page Type meta data.
+	 *
+	 * @return array
+	 */
+	public function meta() {
+		return [
+			'name'        => 'Term test page',
+			'description' => 'Page type to test out the term property'
+		];
+	}
+
+	/**
+	 * Remove meta boxes.
+	 *
+	 * @return array
+	 */
+	public function remove() {
+		return ['editor', 'commentsdiv', 'commentstatusdiv', 'authordiv', 'slugdiv'];
+	}
+
+	/**
+	 * Define our properties.
+	 */
+	public function register() {
+
+		$this->box( 'Content', [
+
+			// Term
+			papi_property( [
+				'type'     => 'term',
+				'title'    => 'Term test',
+				'slug'     => 'term_test',
+				'settings' => [
+					'taxonomy' => ['test_taxonomy', 'test_taxonomy_2'],
+				]
+			] ),
+
+			papi_property( [
+				'type'     => 'term',
+				'title'    => 'Term test 2',
+				'slug'     => 'term_test_2',
+				'settings' => [
+					'taxonomy' => ['test_taxonomy', 'test_taxonomy_2'],
+					'layout'   => 'multiple'
+				]
+			] ),
+
+			papi_property( [
+				'type'     => 'term',
+				'title'    => 'Term test 3',
+				'slug'     => 'term_test_3',
+				'settings' => [
+					'taxonomy' => ['test_taxonomy', 'test_taxonomy_2'],
+					'select2'  => false,
+				]
+			] ),
+
+		] );
+
+	}
+}
