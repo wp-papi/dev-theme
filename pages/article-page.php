bc745d0b674ee00b39ea330236d68fe5f926fb9a
Rasmus Bengtsson <rasmus@isotop.se>
2016-01-02 15:45:57 +0100

    Update article page type

--- pages/article-page.php
+++ pages/article-page.php
@@ -1,25 +1,26 @@
 <?php
-	get_header();
-	while ( have_posts() ) : the_post();
+get_header();
+while ( have_posts() ) : the_post();
 ?>
 
 <div class="entry">
 	<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 	<?php
-		$hero = papi_get_field( 'hero_image' );
-		if ($hero):
-			?>
-			<div class="entry-hero">
-				<img src="<?php echo $hero->url; ?>" />
-			</div>
-			<?php
-		endif;
+	$hero = papi_get_field( 'hero_image' );
+	if ( $hero ):
+		?>
+		<div class="entry-hero">
+			<img src="<?php echo $hero->url; ?>" />
+		</div>
+		<?php
+	endif;
 	?>
 
-	<div class="entry-text">
-		<?php the_papi_field( 'editor' ); ?>
-	</div>
+	<h1>Content</h1>
+	<?php $feature_module = papi_get_field( 'feature_module' ); ?>
+	<h2><?php echo $feature_module->post_title; ?></h2>
+	<?php echo apply_filters( 'the_content', $feature_module->post_content ); ?>
 </div>
 
 <?php
