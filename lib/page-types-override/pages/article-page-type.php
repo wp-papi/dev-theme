1e60cc3006b48ee19b9ed4aa1e4d4329252b95d9
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-02-15 21:38:16 +0100

    Add help tabs and help sidebar to article page

--- lib/page-types-override/pages/article-page-type.php
+++ lib/page-types-override/pages/article-page-type.php
@@ -9,10 +9,42 @@ class Article_Page_Type extends Papi_Page_Type {
 	 */
 	public function meta() {
 		return [
-			'description' => 'Vel adipisicing dapibus nostra. Lectus malesuada volutpat aliquet',
-			'fill_labels' => true,
-			'name'        => 'Article page',
-			'template'    => 'pages/article-page.php'
+			'description'         => 'Vel adipisicing dapibus nostra. Lectus malesuada volutpat aliquet',
+			'fill_labels'         => true,
+			'name'                => 'Article page',
+			'template'            => 'pages/article-page.php',
+			'show_screen_options' => false
+		];
+	}
+	
+	/**
+	 * Get help sidebar information.
+	 *
+	 * @return string
+	 */
+	public function help_sidebar() {
+		return '
+			<p>
+				<strong>For more information:</strong>
+			</p>
+			<p>
+				<a href="https://wp-papi.github.io/" target="_blank">Papi site</a>
+			</p>
+			<p>
+				<a href="https://wp-papi.github.io/docs" target="_blank">Papi docs</a>
+			</p>
+		';
+	}
+	
+	/**
+	 * Add help tabs.
+	 *
+	 * @return array
+	 */
+	public function help() {
+		return [
+			'General'   => 'Varius diam exercitation irure consequat. Facilisis varius id pretium. Risus imperdiet ridiculus',
+			'Page type' => 'Lorem ipsum dadadaa...'
 		];
 	}
 
